294392f85aa4e161093b187cf406577f85cceae1
Tom Pain <hello@tompa.in>
2021-11-29 22:22:30 +0000

    Graphene-Django config
    type: feat
    
    settings.py update, url update, GraphQL Schema, GraphQL queries

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Graphene-Django schema location
+GRAPHENE = {
+  "SCHEMA": "blog.schema.schema",
+}
