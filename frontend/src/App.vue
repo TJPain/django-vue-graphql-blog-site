a8043c140d681a9292624793640af581843c1155
Tom Pain <hello@tompa.in>
2021-12-04 18:12:08 +0000

    Header styling
    Type: feat
    
    Header basic styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
-    <header>
-      <router-link to="/">
-        <h1>My Blog</h1>
-      </router-link>
+    <header class="blog-header__container">
+      <div class="blog-header__wrapper">
+        <router-link to="/">
+          <h1 class="blog_title">Demo Blog</h1>
+        </router-link>
+      </div>
     </header>
-    <router-view />
+    <div class="contents-container">
+      <router-view />
+    </div>
   </div>
 </template>
 
@@ -16,22 +20,81 @@ export default {
 </script>
 
 <style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
 * {
   margin: 0;
   padding: 0;
-}
-
-body {
-  margin: 0;
-  padding: 24px;
 }
 
 * + * {
   margin-top: 24px;
 }
 
-#app {
-  margin: 0;
+.blog-header__container {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  z-index: 1000;
+  width: 100%;
+  background-color: #fff;
+  top: 0;
+  border-bottom: 1px solid #e3e3e3;
+}
+
+.blog-header__wrapper {
+  width: calc(100% - 100px);
   padding: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 83px;
+  width: 100%;
+  max-width: 1170px;
+  margin: 0 auto;
+}
+
+.contents-container {
+  max-width: 1170px;
+  margin: auto;
+  padding-top: 83px;
+}
+
+a,
+a:link,
+a:visited {
+  color: #464860;
+  text-decoration: none;
+}
+
+a.hover {
+  content: "";
+  width: 0;
+  height: 1px;
+  display: block;
+  background: #464860;
+  transition: 0.3s;
+}
+
+.blog_title,
+.blog_title:visited {
+  color: black;
 }
 </style>
