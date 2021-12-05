5ac10771cf78c2f7169825c0f7311b94a064b35c
Tom Pain <hello@tompa.in>
2021-12-05 14:27:37 +0000

    Blog post styling
    Type: feat
    
    Basic styling on the blog post

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="post" v-if="post">
     <h2 class="blog__title">{{ post.title }}: {{ post.subtitle }}</h2>
-    By <AuthorLink :author="post.author" />
-    <div>{{ displayableDate(post.publishDate) }}</div>
+    <div class="blog__details">
+      By <AuthorLink class="author__link" :author="post.author" />
+      <span> on {{ displayableDate(post.publishDate) }}</span>
+    </div>
     <p class="post__description">{{ post.metaDescription }}</p>
     <article>
       {{ post.body }}
     </article>
-    <ul>
-      <li class="post__tags" v-for="tag in post.tags" :key="tag.name">
+    <div class="tags__container">
+      <div class="post__tag" v-for="tag in post.tags" :key="tag.name">
         <router-link :to="`/tag/${tag.name}`">#{{ tag.name }}</router-link>
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -75,6 +77,59 @@ export default {
 }
 
 .blog__title {
-  margin-bottom: 10px;
+  margin-bottom: 8px;
+}
+
+.blog__details {
+  margin-bottom: 30px;
+}
+
+.author__link {
+  color: #3e414d;
+  display: inline-block;
+  text-decoration: unset;
+}
+
+.author__link::after {
+  content: "";
+  width: 0;
+  height: 1px;
+  display: block;
+  background: #3e414d;
+  transition: 300ms;
+}
+
+.author__link:hover::after {
+  width: 100%;
+}
+
+.tags__container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 40px;
+}
+
+.post__tag {
+  list-style: none;
+  font-weight: bold;
+  font-size: 14px;
+  background-color: #6be2d1;
+  border: 1px solid transparent;
+  color: #3e414d;
+  display: inline-block;
+  margin-bottom: 5px;
+  margin-right: 5px;
+  padding: 5px 8px;
+  border-radius: 4px;
+  transition: background-color 0.3s, border-color 0.3s, color 0.3s;
+}
+
+.post__tag:hover {
+  background-color: transparent;
+  border-color: #3e414d;
+  color: #3e414d;
 }
 </style>
